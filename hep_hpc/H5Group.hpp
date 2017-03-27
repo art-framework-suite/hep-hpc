1883e0fb5e78d5ffea422852007331e7adf1a479
Chris Green <artists@fnal.gov>
2017-03-27 15:09:04 -0500

    H5Group and tests.

---
+++ hep_hpc/H5Group.hpp
@@ -0,0 +1,99 @@
+#ifndef hep_hpc_H5Group_hpp
+#define hep_hpc_H5Group_hpp
+
+#include "hep_hpc/H5PropertyList.hpp"
+#include "hep_hpc/HID_t.hpp"
+#include "hep_hpc/SimpleRAII.hpp"
+
+#include "hdf5.h"
+
+namespace hep_hpc {
+  class H5Group;
+}
+
+class hep_hpc::H5Group {
+public:
+  // Group modes:
+  enum group_mode_t : uint8_t { CREATE_MODE, OPEN_MODE, OPEN_OR_CREATE_MODE };
+
+  H5Group() = default;
+
+  // Create or open a group. Note that the H5PropertyList objects (if specified)
+  // may be moved in or copied by value, but anyway do not need to live
+  // beyond this call.
+  H5Group(hid_t fileOrGroup,
+          std::string const & fullPathName,
+          group_mode_t mode = CREATE_MODE,
+          H5PropertyList linkCreationProperties = {},
+          H5PropertyList GroupCreationProperties = {},
+          H5PropertyList GroupAccessProperties = {});
+
+  operator hid_t() const noexcept;
+
+  explicit operator bool () const noexcept;
+
+  // Obtain information about the group;
+  H5G_info_t info() const;
+
+  // Flush the group to disk.
+  void flush();
+
+  // Refresh the group from disk.
+  void refresh();
+
+  // Explicitly close the group.
+  void close();
+
+private:
+  static HID_t const INVALID_GROUP_;
+  SimpleRAII<HID_t> h5group_;
+};
+
+inline
+hep_hpc::H5Group::
+operator hid_t() const noexcept
+{
+  return *h5group_;
+}
+
+inline
+hep_hpc::H5Group::
+operator bool () const noexcept
+{
+  return *h5group_ > INVALID_GROUP_;
+}
+
+inline
+H5G_info_t
+hep_hpc::H5Group::
+info() const
+{
+  H5G_info_t result;
+  (void) H5Gget_info(*h5group_, &result);
+  return result;
+}
+
+inline
+void
+hep_hpc::H5Group::
+flush()
+{
+  (void) H5Gflush(*h5group_);
+}
+
+inline
+void
+hep_hpc::H5Group::
+refresh()
+{
+  (void) H5Grefresh(*h5group_);
+}
+
+inline
+void
+hep_hpc::H5Group::
+close() {
+  h5group_.reset();
+}
+
+#endif /* hep_hpc_H5Group_hpp */
---
+++ test/H5Group_t.cpp
@@ -0,0 +1,122 @@
+#include "hep_hpc/H5File.hpp"
+#include "hep_hpc/H5Group.hpp"
+#include "hep_hpc/errorHandling.hpp"
+#include "hep_hpc/ScopedErrorHandler.hpp"
+
+#include "gtest/gtest.h"
+
+#include <string>
+
+using namespace std::string_literals;
+
+using namespace hep_hpc;
+
+namespace {
+  hep_hpc::H5PropertyList fileAccessProperties()
+  {
+    // Ensure we are using the latest available HDF5 file format to write our data.
+    hep_hpc::H5PropertyList plist(H5P_FILE_ACCESS);
+    H5Pset_libver_bounds(plist, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST);
+    return plist;
+  }
+}
+
+TEST(H5Group, def)
+{
+  H5Group const g;
+  ASSERT_FALSE(g);
+}
+
+TEST(H5Group, construct)
+{
+  H5File const h("h5group_t.hdf5", H5F_ACC_TRUNC, {}, fileAccessProperties());
+  H5Group g(h, "/G1"s);
+  ASSERT_TRUE(g);
+}
+
+TEST(H5Group, move_construction)
+{
+  H5File const h("h5group_t.hdf5", H5F_ACC_TRUNC, {}, fileAccessProperties());
+  H5Group g(h, "/G1"s);
+  ASSERT_TRUE(g);
+  H5Group const g2(std::move(g));
+  ASSERT_FALSE(g);
+  ASSERT_TRUE(g2);
+}
+
+TEST(H5Group, move_assignment)
+{
+  H5File const h("h5group_t.hdf5", H5F_ACC_TRUNC, {}, fileAccessProperties());
+  H5Group g(h, "/G1"s);
+  ASSERT_TRUE(g);
+  H5Group g2;
+  g2 = std::move(g);
+  ASSERT_FALSE(g);
+  ASSERT_TRUE(g2);
+}
+
+TEST(H5Group, open_success)
+{
+  H5File h("h5group_t.hdf5");
+  H5Group g(h, "/G1"s, H5Group::OPEN_MODE);
+  ASSERT_TRUE(g);
+}
+
+TEST(H5Group, open_missing)
+{
+  H5File h("h5group_t.hdf5");
+  ScopedErrorHandler seh;
+  H5Group g(h, "/G2"s, H5Group::OPEN_MODE);
+  ASSERT_FALSE(g);
+}
+
+TEST(H5Group, open_or_create)
+{
+  H5File h("h5group_t.hdf5", H5F_ACC_RDWR);
+  H5Group g(h, "/G2"s, H5Group::OPEN_OR_CREATE_MODE);
+  ASSERT_TRUE(g);
+}
+
+TEST(H5Group, info)
+{
+  H5File h("h5group_t.hdf5", H5F_ACC_RDWR);
+  H5Group g(h, "/G1"s, H5Group::OPEN_MODE);
+  ASSERT_TRUE(g);
+  ASSERT_EQ(g.info().nlinks, 0ull);
+  H5Group g3(g, "G3"s, H5Group::CREATE_MODE);
+  ASSERT_TRUE(g3);
+  ASSERT_EQ(g.info().nlinks, 1ull);
+}
+
+TEST(H5Group, flush)
+{
+  H5File const h("h5group_t.hdf5", H5F_ACC_TRUNC, {}, fileAccessProperties());
+  H5Group g(h, "/G1"s);
+  ASSERT_TRUE(g);
+  g.flush();
+  ASSERT_TRUE(g);
+}
+
+TEST(H5Group, refresh)
+{
+  H5File const h("h5group_t.hdf5", H5F_ACC_RDWR);
+  H5Group g(h, "/G1"s, H5Group::OPEN_MODE);
+  ASSERT_TRUE(g);
+  g.refresh();
+  ASSERT_TRUE(g);
+}
+
+TEST(H5Group, explicit_close)
+{
+  H5File const h("h5group_t.hdf5", H5F_ACC_RDWR);
+  H5Group g(h, "/G1"s, H5Group::OPEN_MODE);
+  ASSERT_TRUE(g);
+  g.close();
+  ASSERT_FALSE(g);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  initH5ErrorHandling();
+  return RUN_ALL_TESTS();
+}
