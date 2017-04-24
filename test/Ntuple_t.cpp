9b4835fd3f92accb8bab33e29135e32832a625c9
Chris Green <artists@fnal.gov>
2017-04-24 12:50:18 -0500

    Change column names to avoid confusion with typespecs.

--- test/Ntuple_t.cpp
+++ test/Ntuple_t.cpp
@@ -10,7 +10,7 @@ int main()
   ErrorController::setErrorHandler(ErrorMode::EXCEPTION);
   Ntuple<int, double, Column<int, 2>, char const *>
     data("test-ntuple.hdf5", "g1",
-         {{"I1"s, 2}, "D1"s, {"I2"s, {2, 3}}, {"S1"s, 2}},
+         {{"A"s, 2}, "B"s, {"C"s, {2, 3}}, {"D"s, 2}},
          2);
   int i1data[] = { 1, 1, 2, 4, 3, 6, 5, 10, 7, 14, 11, 22, 13, 26, 17, 34, 23, 46};
   double d1data[] = { 1.01, 2.02, 3.03, 5.05, 7.07, 11.11, 13.13, 19.17, 23.23 };
