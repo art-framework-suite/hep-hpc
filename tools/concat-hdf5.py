4d69e5733129d95dbb1b7e3b378313944b2d94c7
Chris Green <artists@fnal.gov>
2017-11-21 11:24:00 -0600

    Operational version of file concatenator.

--- tools/concat-hdf5.py
+++ tools/concat-hdf5.py
@@ -13,27 +13,25 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Tabular file concatenator.')
-    parser.add_argument('--output', '-o', help='Name of HDF5 output file.')
+    parser.add_argument('--output', '-o', help='Name of HDF5 output file (obligatory option argument).')
     parser.add_argument('--append', help='Append to an existing HDF5 file.', action='store_true', default=False)
+    parser.add_argument('--filename-column', help='Add a column whose value is extracted from the full input path by the provided regex replacement (to groups matching regexes only, if provided): args = <column-name> [<regex> [<replacement-expression> [<group-regex>+]]].', nargs='+')
     parser.add_argument('--mem-max', help='Maximum amount of memory to use to buffer input (MiB).', type=int, default=100)
-    parser.add_argument('--filename-column', help='Add a column whose value is extracted from the full input path by the provided regex replacement: args = <column-name> [<regex> [<replacement-expression>]].', nargs='+')
+    parser.add_argument('--only-groups', help='Handle only groups matching these regexes, and the datasets within them.',
+                        nargs='+')
     parser.add_argument('-v', help='Verbose: repeat for higher verbosity levels', action='count')
     parser.add_argument('inputs', help='Input files to concatenate (use -- to separate from option arguments if necessary)', nargs='+')
     return parser.parse_args()
 
 class HDF5FileConcatenator:
-    def __init__(self, output, append, mem_max, filename_column, verbose):
+    def __init__(self, output, append, mem_max, filename_column, only_groups, verbose):
         self._mem_max_bytes = mem_max * 1024 * 1024
         self._filename_column = filename_column
         self._verbose = verbose
-
-        if append:
-            mode = 'a'
-        else:
-            mode = 'w-'
-
-        self._output_file = h5py.File(output, mode)
+        self._output_file = h5py.File(output, 'a' if append else 'w-')
         self._ds_out = {}
+        # Need tuple keyword to avoid unwanted generator expression!
+        self._only_groups = tuple(re.compile(r, re.DEBUG if self._verbose > 3 else 0) for r in only_groups) if only_groups else ()
 
     def __enter__(self):
         return self;
@@ -46,56 +44,74 @@ class HDF5FileConcatenator:
         if self._filename_column is not None:
             fn_regex = re.compile(self._filename_column[1]) if len(self._filename_column) > 1 else None
             filename_column_values = [ fn_regex.sub(self._filename_column[2] if len(self._filename_column) > 2 else "", f) for f in file_list ] if fn_regex is not None else file_list
-            maxchars = max([ len(x) for x in filename_column_values ])
+            maxchars = max(len(x) for x in filename_column_values)
+
+            filename_column_group_patterns \
+                = tuple(re.compile(r) for r in self._filename_column[3:]) \
+                if len(self._filename_column) > 3 else ()
 
         # Iterate over files:
         for input_file_name in file_list:
             self._groups_with_datasets = {}
             filename_column_value = filename_column_values.pop(0) if self._filename_column is not None else None
-            if self._verbose > 1 and self._filename_column is not None:
-                print("Calculated value for filename column dataset {} of {} for input file {}.".format(self._filename_column[0], filename_column_value, input_file_name))
+            if self._verbose > 2 and self._filename_column is not None:
+                print("DEBUG(3): Calculated value for filename column dataset {} of {} for input file {}.".format(self._filename_column[0], filename_column_value, input_file_name))
             # 1. Open input file.
             input_file = h5py.File(input_file_name, 'r')
             # 2. Discover and iterate over items.
+            print("INFO: Processing input file {}.".format(input_file_name))
             input_file.visititems(self._visit_item)
             # 3. For each group in the output file with datasets with
             #    new rows, populate the appropriate number of rows in
             #    the filename column, if we have one.
             if filename_column_value is not None:
                 for ds_group_name in self._groups_with_datasets.keys():
+                    if filename_column_group_patterns \
+                       and not any(r.match(ds_group_name) for r in filename_column_group_patterns):
+                        continue
                     ds_group = self._output_file[ds_group_name]
                     if self._filename_column[0] not in ds_group: # Need output filename column
+                        if self._verbose > 0:
+                            print("DEBUG(1): Creating filename_column dataset {} in group {}.".format(self._filename_column[0], ds_group_name))
                         ds_group.create_dataset(self._filename_column[0],
-                                                (0),
+                                                (0,),
                                                 "S{}".format(maxchars),
-                                                maxshape = (None),
+                                                maxshape = (None,),
                                                 chunks = True,
                                                 compression = "gzip",
                                                 compression_opts = 6)
                     filename_column = ds_group[self._filename_column[0]]
                     filename_column_len = filename_column.len()
-                    new_rows = max([ds.len() if type(ds) == h5py.Dataset else 0 for ds in ds_group.values()]) - \
+                    new_rows = max(ds.len() if type(ds) == h5py.Dataset else 0 for ds in ds_group.values()) - \
                                filename_column_len
-                    filename_column.resize(filename_column_len + new_rows)
+                    filename_column.resize(filename_column_len + new_rows, 0)
                     filename_column[filename_column_len : filename_column_len + new_rows] = filename_column_value
 
     def _visit_item(self, name, obj):
         # Group or dataset?
         if type(obj) == h5py.Group:
+            if self._only_groups and not any(r.match(obj.name) for r in self._only_groups):
+                if self._verbose > 2:
+                    print("DEBUG(3): Ignoring group {} due to failure to match only_groups regexes.".format(obj.name))
+                return
             # Ensure the output file has a corresponding group.
             if self._verbose > 1:
-                print("Ensuring existence of group {} in output file.".format(name))
+                print("DEBUG(2): Ensuring existence of group {} in output file.".format(obj.name))
             self._output_file.require_group(name)
         elif type(obj) == h5py.Dataset:
+            if self._only_groups and not any(r.match(obj.parent.name) for r in self._only_groups):
+                if self._verbose > 2:
+                    print("DEBUG(3): Ignoring dataset {} due to failure to match only_groups regexes for group {}.".format(obj.name, obj.parent.name))
+                return
             if self._verbose > 0:
-                print("Found dataset {} with {} rows.".format(name, obj.len()))
+                print("DEBUG(1): Found dataset {} with {} rows.".format(obj.name, obj.len()))
             if self._filename_column is not None and \
                string.split(obj.name, '/')[-1] == self._filename_column[0]:
                 raise RuntimeError("Input dataset {} clashes with specified filename column.".format(obj.name))
             self._groups_with_datasets[obj.parent.name] = True
-            self._handle_dataset(name, obj)
+            self._handle_dataset(obj.name, obj)
         else:
-            print("Ignoring unrecognized item with name {} and type {}".format(name, obj.__class__.name__))
+            print("INFO: Ignoring unrecognized item with name {} and type {}".format(name, obj.__class__.name__))
 
     def _handle_dataset(self, path, ds_in):
         # 3. Discover dataset shape and row size.
@@ -108,33 +124,40 @@ class HDF5FileConcatenator:
             max_shape[0] = None
             new_shape = list(shape)
             new_shape[0] = 0
-            self._ds_out[path] = { "ds" : self._output_file.create_dataset(path, new_shape, ds_in.dtype,
-                                                                           chunks = ds_in.chunks,
-                                                                           compression = ds_in.compression,
-                                                                           compression_opts = ds_in.compression_opts,
-                                                                           maxshape = max_shape,
-                                                                           shuffle = ds_in.shuffle,
-                                                                           fletcher32 = ds_in.fletcher32,
-                                                                           scaleoffset = ds_in.scaleoffset,
-                                                                           fillvalue  = ds_in.fillvalue),
-                                   "row_size_bytes" : ds_in.size / ds_in.len() * ds_in.dtype.itemsize,
+            self._ds_out[path] = { "ds" : self._output_file.\
+                                   create_dataset(path, new_shape, ds_in.dtype,
+                                                  chunks = ds_in.chunks,
+                                                  compression = ds_in.compression,
+                                                  compression_opts = ds_in.compression_opts,
+                                                  maxshape = max_shape,
+                                                  shuffle = ds_in.shuffle,
+                                                  fletcher32 = ds_in.fletcher32,
+                                                  scaleoffset = ds_in.scaleoffset,
+                                                  fillvalue  = ds_in.fillvalue),
+                                   "row_size_bytes" : ds_in.size / \
+                                   ds_in.len() * ds_in.dtype.itemsize,
                                }
-            self._ds_out[path]["buffer_size_rows"] = self._mem_max_bytes / self._ds_out[path]["row_size_bytes"]
+            self._ds_out[path]["buffer_size_rows"] \
+                = self._mem_max_bytes / self._ds_out[path]["row_size_bytes"]
             if ds_in.attrs:
                 self._ds_out[path]["ds"].attrs = ds_in.attrs
 
         # 5. Iterate over buffer-sized chunks.
         while n_rows_written < len_in:
             output_current_size = self._ds_out[path]["ds"].len()
-            rows_to_write = min(ds_in.len(), self._ds_out[path]["buffer_size_rows"])
+            rows_to_write = min(ds_in.len() - n_rows_written, \
+                                self._ds_out[path]["buffer_size_rows"])
             # 6. Resize the output dataset appropriately.
             if self._verbose > 1:
-                print("Resize {} from {} to {} rows.".format(path, output_current_size, output_current_size + rows_to_write))
+                print("DEBUG(2): Resize {} from {} to {} rows.".\
+                      format(path, output_current_size, \
+                             output_current_size + rows_to_write))
             self._ds_out[path]["ds"].resize(output_current_size + rows_to_write, 0)
             # 7. Write the output slice in the appropriate place 
             if self._verbose > 1:
-                print("Writing {} rows to dataset {}.".format(rows_to_write, path))
-            self._ds_out[path]["ds"][output_current_size : output_current_size + rows_to_write, ...] = ds_in[n_rows_written : n_rows_written + rows_to_write, ...]
+                print("DEBUG(2): Writing {} rows to dataset {}.".format(rows_to_write, path))
+            self._ds_out[path]["ds"][output_current_size : output_current_size + rows_to_write, ...] \
+                = ds_in[n_rows_written : n_rows_written + rows_to_write, ...]
             # Update cursor.
             n_rows_written += rows_to_write
 
@@ -153,6 +176,7 @@ if __name__ == "__main__":
                               args.append,
                               args.mem_max,
                               args.filename_column,
+                              args.only_groups,
                               args.v) as concatenator:
         concatenator.concatFiles(args.inputs)
 
