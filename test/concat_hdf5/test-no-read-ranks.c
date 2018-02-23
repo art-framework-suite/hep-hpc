b105bbcd28ef2b27e6e7ec303a2c832440a81bc6
Chris Green <artists@fnal.gov>
2018-02-23 15:09:41 -0600

    Tweaks and read test.

---
+++ test/concat_hdf5/test-no-read-ranks.c
@@ -0,0 +1,135 @@
+/***********************************************************************
+* test-no-read-ranks
+*
+* Demonstrate what happens with collective reads when a rank has
+* nothing to write.
+*
+* Requires a file written by test-no-write-ranks.c.
+*
+***********************************************************************/
+
+#include "mpi.h"
+#include "hdf5.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int n_ranks;
+static int my_rank;
+
+static bool want_visit = false;
+
+static
+herr_t
+visit_item(hid_t root_id,
+           char const * obj_name,
+           H5O_info_t const * obj_info,
+           void * data)
+{
+  // Find the dataset we want and open it.
+  if (strcmp("dset", obj_name) == 0) {
+    /* Open the dataset. */
+    *(hid_t *)data = H5Oopen_by_addr(root_id, obj_info->addr);
+  }
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+  /* Initialize MPI */
+  int const required = MPI_THREAD_FUNNELED;
+  int provided;
+  herr_t status = MPI_Init_thread(&argc, &argv, required, &provided);
+  if (status != MPI_SUCCESS || required != provided) {
+    fprintf(stderr, "ERROR: unable to initialize MPI.\n");
+    exit(2);
+  }
+
+  if (argc == 2 && strcmp(argv[1], "--visit") == 0) {
+    printf("Selected visitor operation.\n");
+    want_visit = true;
+  }
+
+  MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+  hsize_t const chunk_size = 7ull;
+
+  /* Open input file as created by test-no-write-ranks.c. */
+  hid_t fprop = H5Pcreate(H5P_FILE_ACCESS);
+  status = H5Pset_fapl_mpio(fprop, MPI_COMM_WORLD, MPI_INFO_NULL);
+  hid_t file = H5Fopen("test-no-write-ranks.hdf5", H5F_ACC_RDONLY, fprop);
+
+  /* Open the dataset, one way or the other. */
+  hid_t in_dataset = -1;
+  if (want_visit) {
+    H5Ovisit(file, H5_INDEX_NAME, H5_ITER_NATIVE, &visit_item, &in_dataset);
+  } else {
+    in_dataset = H5Dopen2(file, "dset", H5P_DEFAULT);
+  }
+  if (in_dataset < 0) {
+    fprintf(stderr, "Unable to find / open expected dataset \"dset\".\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  /* To hold incoming data. */
+  int data[chunk_size][2][3];
+  memset(data, 0, chunk_size * 2 * 3 * sizeof(int));
+
+  hid_t in_dataspace = H5Dget_space(in_dataset);
+  hid_t mem_dspace;
+
+  hsize_t const ndims = H5Sget_simple_extent_ndims(in_dataspace);
+  if (ndims != 3) {
+    fprintf(stderr, "ERROR: dset has unexpected rank!\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  } 
+  hsize_t const n_elements_to_read[3] = {chunk_size, 2, 3};
+  hsize_t n_elements[3], dummy[3];
+  status =
+    H5Sget_simple_extent_dims(in_dataspace, n_elements, dummy);
+  if (memcmp(n_elements_to_read + 1, n_elements + 1, 2 * sizeof(hsize_t)) != 0) {
+    fprintf(stderr, "ERROR: dset has unexpected extents: (%llu, %llu %llu)!\n",
+            n_elements[0], n_elements[1], n_elements[2]);
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  if (my_rank == 1 || n_elements[0] <= my_rank * chunk_size) { /* Don't read anything here. */
+    printf("NOP read for rank %d\n", my_rank);
+    H5Sselect_none(in_dataspace);
+    mem_dspace = H5Screate(H5S_NULL);
+  } else {
+    hsize_t offsets[3] = {my_rank * chunk_size, 0, 0};
+    hsize_t block_count[3] = {1, 1, 1};
+    H5Sselect_hyperslab(in_dataspace,
+                        H5S_SELECT_SET,
+                        offsets,
+                        NULL,
+                        block_count,
+                        n_elements_to_read);
+    mem_dspace = H5Screate_simple(3, n_elements_to_read,  n_elements_to_read);
+  }
+
+  /* Set up collective read/write operations. */
+  hid_t xprop = H5Pcreate(H5P_DATASET_XFER);
+  status = H5Pset_dxpl_mpio(xprop, H5FD_MPIO_COLLECTIVE);
+
+  /* Read the data. */
+  status = H5Dread(in_dataset,
+                   H5T_NATIVE_INT,
+                   mem_dspace,
+                   in_dataspace,
+                   xprop,
+                   data);
+
+  status = H5Dclose(in_dataset);
+  status = H5Sclose(in_dataspace);
+  status = H5Sclose(mem_dspace);
+  status = H5Pclose(fprop);
+  status = H5Pclose(xprop);
+  status = H5Fclose(file);
+
+  MPI_Finalize();
+  return status;
+}
--- test/concat_hdf5/test-no-write-ranks.c
+++ test/concat_hdf5/test-no-write-ranks.c
@@ -1,14 +1,15 @@
-////////////////////////////////////////////////////////////////////////
-// test-no-write-ranks 
-//
-// Demonstrate what happens with collective writes when a rank has
-// nothing to write.
-//
-////////////////////////////////////////////////////////////////////////
+/***********************************************************************
+* test-no-write-ranks
+*
+* Demonstrate what happens with collective writes when a rank has
+* nothing to write.
+*
+***********************************************************************/
 
 #include "mpi.h"
 #include "hdf5.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 
 static int n_ranks;
@@ -16,11 +17,12 @@ static int my_rank;
 
 int main(int argc, char **argv)
 {
-  // Initialize MPI
+  /* Initialize MPI. */
   int const required = MPI_THREAD_FUNNELED;
   int provided;
   herr_t status = MPI_Init_thread(&argc, &argv, required, &provided);
   if (status != MPI_SUCCESS || required != provided) {
+    fprintf(stderr, "ERROR: unable to initialize MPI.\n");
     exit(2);
   }
 
@@ -30,25 +32,25 @@ int main(int argc, char **argv)
   hsize_t const chunk_size = 7ull;
   hsize_t const n_chunks = n_ranks;
 
-  // Create an N x 2 x 3 dataset.
+  /* Create an N x 2 x 3 dataset. */
   hsize_t dims[3] = {chunk_size * n_chunks, 2, 3};
   hsize_t maxdims[3] = {H5S_UNLIMITED, 2, 3};
   hsize_t chunk_dims[3] = {chunk_size, 2, 3};
 
-  // Create a dataspace.
+  /* Create a dataspace. */
   hid_t dataspace = H5Screate_simple(3, dims, maxdims);
 
-  // Create a new file (overwrite if it already exists).
+  /* Create a new file (overwrite if it already exists). */
   hid_t fprop = H5Pcreate(H5P_FILE_ACCESS);
   status = H5Pset_fapl_mpio(fprop, MPI_COMM_WORLD, MPI_INFO_NULL);
-  hid_t file = H5Fcreate("test.hdf5", H5F_ACC_TRUNC, H5P_DEFAULT, fprop);
+  hid_t file = H5Fcreate("test-no-write-ranks.hdf5", H5F_ACC_TRUNC, H5P_DEFAULT, fprop);
 
-  // Enable chunking, compression.
+  /* Enable chunking, compression. */
   hid_t prop = H5Pcreate(H5P_DATASET_CREATE);
   status = H5Pset_chunk(prop, 3, chunk_dims);
   status = H5Pset_deflate(prop, 6);
 
-  // Create dataset.
+  /* Create dataset. */
   hid_t dataset = H5Dcreate2(file, "dset", H5T_NATIVE_INT, dataspace,
                              H5P_DEFAULT, prop, H5P_DEFAULT);
 
@@ -65,7 +67,7 @@ int main(int argc, char **argv)
 
   hsize_t n_elements[3] = {chunk_size, 2, 3};
 
-  if (my_rank == 1) { // Pretend we have nothing to write here.
+  if (my_rank == 1) { /* Pretend we have nothing to write here. */
     H5Sselect_none(dataspace);
     mem_dspace = H5Screate(H5S_NULL);
   } else {
